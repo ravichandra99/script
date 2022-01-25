d2a1d7c231490e4a4fc873bd6abf092fcb0cda3f
ravichandra99 <52348499+ravichandra99@users.noreply.github.com>
2022-01-25 12:44:21 +0530

    Update screenly-ose.sh

--- screenly-ose.sh
+++ screenly-ose.sh
@@ -121,10 +121,10 @@ fi
 if [ -z "${REPOSITORY}" ]; then
   if [ "$WEB_UPGRADE" = false ]; then
     set -x
-    REPOSITORY=${1:-https://github.com/ravichandra99/screenly-ose.git}
+    REPOSITORY=${1:-https://github.com/ravichandra99/screenly-ose-test.git}
   else
     set -e
-    REPOSITORY=https://github.com/ravichandra99/screenly-ose.git
+    REPOSITORY=https://github.com/ravichandra99/screenly-ose-test.git
   fi
 fi
 
