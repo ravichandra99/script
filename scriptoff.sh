33cabfa7158a27504f0b5b8af506830ec393c372
ravichandra99 <52348499+ravichandra99@users.noreply.github.com>
2022-04-06 11:30:56 +0530

    Update scriptoff.sh

--- scriptoff.sh
+++ scriptoff.sh
@@ -161,7 +161,7 @@ fi
 
 # Install Ansible from requirements file.
 if [ "$BRANCH" = "master" ]; then
-    ANSIBLE_VERSION=$(curl -s https://raw.githubusercontent.com/ravichandra99/screenly-ose-test/production/requirements/requirements.host.txt | grep ansible)
+    ANSIBLE_VERSION=$(curl -s https://raw.githubusercontent.com/ravichandra99/screenly-ose/production/requirements/requirements.host.txt | grep ansible)
 else
     ANSIBLE_VERSION=2.10.7
 fi
